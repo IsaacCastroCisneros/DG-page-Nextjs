9b5a624b14057fecc42f45160d70c6dc9af75f78
isaac <isaaccastrocisneros@gmail.com>
2023-09-01 12:26:55 -0500

    last last

--- components/Navbar/components/DropDownItem.tsx
+++ components/Navbar/components/DropDownItem.tsx
@@ -12,8 +12,8 @@ interface props
   type:string,
   onClick?:()=>void,
   mob?:boolean,
-  icon:string,
-  tag:string
+  icon?:string,
+  tag?:string
 }
 
 export const DropDownItem=(props:props)=> 
@@ -32,7 +32,7 @@ export const DropDownItem=(props:props)=>
   
   return (
     <div className="flex gap-[.5rem] items-center">
-      <Image src={type==="cursos"? icon:img} width={50} height={50} alt="DG-icon-programa" className='h-[50px] w-[50px] rounded-[100%] border-[1px] border-myGrey' />
+      <Image src={type==="cursos"? icon||'':img} width={50} height={50} alt="DG-icon-programa" className='h-[50px] w-[50px] rounded-[100%] border-[1px] border-myGrey' />
       <p className="flex flex-col">
         {!mob && (
           <Menu.Item>
