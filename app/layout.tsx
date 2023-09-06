47c38f392a7056aacad321c757a5d56e99b0155e
isaac <isaaccastrocisneros@gmail.com>
2023-09-06 11:42:44 -0500

    last

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { GlobalContext } from '@/context/GlobalContext';
 import { TheMsg, MobCart,MobNoti } from ".";
 import getRequest from '@/helpers/getRequest';
+import Script from 'next/script';
 
 export default async function RootLayout({
   children,
@@ -20,16 +21,23 @@ export default async function RootLayout({
 
   return (
     <html lang="es">
+      <Script src="//code.jivosite.com/widget/5kqbbZ9lmq" async />
+      <Script src="//code.jivosite.com/widget/5kqbbZ9lmq" async />
+      <Script src="//code.tidio.co/t0h64oj6c2cfkny7omjodxcxdvv9gyv3.js" async />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-V810B1M8GG"
+      />
       <GlobalContext
-        cursos={errC ? []:cursos}
-        diplomas={errD ? []:diplomas}
-        >
+        cursos={errC ? [] : cursos}
+        diplomas={errD ? [] : diplomas}
+      >
         <body>
-          <TheMsg/>
-          <MobCart/>
-          <MobNoti/>
+          <TheMsg />
+          <MobCart />
+          <MobNoti />
           <header>
-            <Navbar/>
+            <Navbar />
           </header>
           <main>{children}</main>
           <Footer />
