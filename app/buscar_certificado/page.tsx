f408a92e12cc59f027aace31845a7cd063d84c70
isaac <isaaccastrocisneros@gmail.com>
2023-09-11 17:01:11 -0500

    hello

--- app/buscar_certificado/page.tsx
+++ app/buscar_certificado/page.tsx
@@ -1,24 +1,43 @@
 "use client"
 
 import { MyPopUp } from '@/components/MyPopUp/MyPopUp'
+import { globalContext } from '@/context/GlobalContext'
 import axios from 'axios'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState,useContext, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface row
+{
+  cod_registro_0:     string;
+  cod_registro_1:     string;
+  nombres:            string;
+  dni:                string;
+  categoria:          string;
+  programa:           string;
+  mes:                string;
+  year:               string;
+  horas_certificadas: string;
+  fecha_inicio:       string;
+  fecha_fin:          string;
+  estado:             string;
+}
 
 export default function Page({searchParams}:any) 
 {
     const[values,setValues]=useState<string>("")
-    const[data,setData]=useState<any>(false)
+    const[data,setData]=useState<Array<row>>([])
     const[show,setShow]=useState<boolean>(false)
     const{search}=searchParams
+    const{setShowMsg}=useContext(globalContext)
 
 
     useEffect(()=>
     {
         if(search)
         {
-            const myEntry = search.split('=')[1]||''
-            if(myEntry.includes('EDGG'))
-            {
+          if(search.includes('EDGG'))
+          {
+              console.log('sii')
                submitData(search)
             }
         }
@@ -32,17 +51,21 @@ export default function Page({searchParams}:any)
 
     async function submitData(values:string)
     {
-        if(values.length<4)return
+        if(values.length<4)return setShowMsg({show:true,type:"fail",content:"El DNI o codigo no es correcto"})
 
         const form=new FormData()
         form.append("text",values)
  
         const res= await axios.post('https://aula.desarrolloglobal.pe/v03/api/certificados',form)
-        if(res)
+        if(res.data)
         {
           setData(res.data)
           setShow(true)
+          return
         }
+      
+        setShowMsg({show:true,type:"fail",content:"El DNI o codigo no es correcto"})
+
     }
 
     return (
@@ -78,23 +101,52 @@ export default function Page({searchParams}:any)
     );
 }
 
-function Table({data}:{data:any} )
+function Table({data}:{data:Array<row>} )
 {
+    function Cell({children,className=""}:{children:ReactNode,className?:string})
+    {
+      return(
+        <strong className={twMerge('text-center px-[2rem] py-[1rem] flex-[1] block',className)}>{children}</strong>
+      )
+    }
+
+    function Block(props:row)
+    {
+      const{cod_registro_0,cod_registro_1,nombres,categoria,programa,mes,year,estado}=props
+
+      const myEstado = estado === "entregado" ? (<>Entregado</>) : estado === "aula" ? (<>En Aula Virtual</>) : (<>En Oficina</>)
+
+      return(
+        <div className='flex w-[100%] border-b-[1px] border-[#dee2e6]'>
+           <Cell>{cod_registro_0}{cod_registro_1}</Cell>
+            <Cell className='flex-[2]'>{nombres}</Cell>
+            <Cell>{categoria}</Cell>
+            <Cell>{programa}</Cell>
+            <Cell>{mes}/{year}</Cell>
+            <Cell>{myEstado}</Cell>
+        </div>
+      )
+    }
+
     return (
-      <div className="bg-white">
+      <div className="bg-white w-[1137px] max-w-[100%] overflow-x-auto">
         <strong className="text-red-500 px-[1.4rem] py-[1.2rem] text-[1.3rem] block border-[1px] border-[#dee2e6]">
           Certificados Verificados
         </strong>
-        <thead>
-          <tr className="tablet">
-            <th>CODIGO</th>
-            <th>NOMBRES</th>
-            <th>CATEGORIA</th>
-            <th>PROGRAMA</th>
-            <th>MES/AÑO</th>
-            <th>ESTADO</th>
-          </tr>
-        </thead>
+        <div className='flex w-[100%] border-[#dee2e6] border-b-[1px]'>
+            <Cell>CODIGO</Cell>
+            <Cell className='flex-[2]'>NOMBRES</Cell>
+            <Cell>CATEGORIA</Cell>
+            <Cell>PROGRAMA</Cell>
+            <Cell>MES/AÑO</Cell>
+            <Cell>ESTADO</Cell>
+        </div>
+        {
+          data.map((data,pos)=>
+          (
+             <Block key={pos} {...data} />
+          ))
+        }
       </div>
     );
 }
